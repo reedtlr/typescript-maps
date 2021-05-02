8d551e8849afb301dfbf514e1ab2d3fb05df786a
Reed <reedwtaylor@gmail.com>
2021-05-02 08:05:57 -0400

    add google maps

--- SRC/index.ts
+++ SRC/index.ts
@@ -1,9 +1,18 @@
 import { User } from './User';
 import { Company } from './Company';
 
-
 const user = new User();
 console.log(user);
 
 const company = new Company();
 console.log(company);
+
+new google.maps.Map(document.getElementById('map'), {
+    zoom: 1,
+    center: {
+        lat: 0,
+        lng: 0
+    }
+});
+
+
